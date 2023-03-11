f59ca39773dc73ab738503c149d31215f426eb49
SivakumarRevuri <sivakumarr96@gmail.com>
2023-03-11 07:30:01 +0530

    11th March 2023

--- django/hello_django/hello_project/settings.py
+++ django/hello_django/hello_project/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-print(STATIC_DIR)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'hello_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sql_practice',
+        'HOST': 'localhost',
+        'USERNAME': 'root',
+        'PASSWORD': 'welcome@1'
     }
 }
 
@@ -120,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_DIRS = [
+STATICFILE_DIRS = [
     STATIC_DIR,
 ]
 
--- django/hello_django/testapp/models.py
+++ django/hello_django/testapp/models.py
@@ -6,4 +6,14 @@ class Employee(models.Model):
     ename = models.CharField(max_length=64)
     esalary = models.FloatField()
     eaddress = models.CharField(max_length=64)
-    
+
+
+class emp(models.Model):
+    empno = models.IntegerField(primary_key=True)
+    ename = models.CharField(10)
+    job = models.CharField(9)
+    mgr = models.IntegerField() 
+    hiredate = models.DateField()
+    sal = models.FloatField()
+    comm = models.FloatField()
+    deptno = models.IntegerField()
