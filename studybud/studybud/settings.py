810d5b313ffd082531224ae0d6e45cde7f0f6cda
Sivakumar Revuri <sivakumarr96@gmail.com>
2023-04-22 21:42:04 +0530

    22nd April 2023

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'studybuddy',
         'USER': 'root',
-        'PASSWORD': 'welcome@1',
+        'PASSWORD': 'Aug@2021',
         'HOST': 'localhost',
         'PORT': '3306'
     }
